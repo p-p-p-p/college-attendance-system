8d810a204932080a531c978883792b0f6c8d9063
p-p-p-p <81347975+p-p-p-p@users.noreply.github.com>
2023-05-19 15:21:48 +0530

    Update 2_Model_Building.py

--- pages/2_Model_Building.py
+++ pages/2_Model_Building.py
@@ -5,6 +5,10 @@ import plotly.graph_objects as go
 import os
 import csv
 import numpy as np
+try:
+    os.remove("database.csv")
+except:
+    pass
 st.markdown("<h1 style='text-align: center;'>Train Model</h1>", unsafe_allow_html=True)
 
 if not os.path.exists("database.sqlite"):
@@ -114,7 +118,10 @@ def run_model():
     X_train, X_test, y_train, y_test = train_test_split(df["face_data"], df["id_number"], test_size=0.2, random_state=42)
 
     # Create the SVM model
-    clf = svm.SVC(kernel='linear')
+    # clf = svm.SVC(kernel='linear')
+    clf = svm.SVC(kernel='poly', degree=3)
+    # clf = svm.SVC(kernel='rbf')
+    # clf = svm.SVC(kernel='sigmoid')
 
     # Train the model on the training set
     clf.fit(list(X_train), list(y_train))
