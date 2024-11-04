c96df5d66329cd9ac0c5e614b0ff231ccd24add7
p-p-p-p <81347975+p-p-p-p@users.noreply.github.com>
2024-11-04 17:30:46 +0530

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,5 +1,5 @@
 import os
-revoked_list = ["database.csv","database.sqlite","name.sqlite","attendance.sqlite","attendance_report.csv","attendance_report.xlsx","database.csv"]
+revoked_list = ["database.csv","database.sqlite","name.sqlite","attendance.sqlite","attendance_report.csv","attendance_report.xlsx","database.csv","svm_model.pkl"]
 for i in revoked_list:
     try:
         os.remove(i)
